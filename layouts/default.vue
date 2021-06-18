bc0f17f6ced0c98049ab69f6b582a47d4544aba7
Sanket <sanket.pahuja@gida.in>
2021-06-18 12:32:52 +0530

    asd

---
+++ layouts/default.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="container">
+    <main role="main">
+      <nuxt/>
+    </main>
+  </div>
+</template>
+
+<script>
+  import Greeting from '~/components/greeting.vue'
+
+  export default {
+    components: {
+      Greeting
+    }
+  }
+</script>
+
+<style>
+  
+  .header {
+    background: #F7F9FA;
+  }
+
+  .page-bar {
+    max-width: 100%;
+    padding: 2em 10%;
+  }
+
+  .tag-page.header h2 {
+    text-align: center;
+    padding: 2.5em 0;
+    color: #A0A0A0;
+  }
+
+  img {
+    display: block;
+    width: 100%;
+  }
+
+  h1,
+  h2,
+  h3 {
+    font-size: 2em;
+  }
+
+  /* Copy section */
+  .body-container {
+    background: #ffffff;
+  }
+
+  .items-list {
+    overflow: hidden;
+  }
+
+  .item {
+    padding: 1em 3em 1em 0;
+  }
+
+  @media all and (min-width: 600px) {
+    .item {
+      float: left;
+      width: 50%;
+    }
+  }
+
+  @media all and (min-width: 900px) {
+    .item {
+      width: 33.333%;
+    }
+  }
+
+  /* Shared */
+
+  .tiny {
+    text-transform: uppercase;
+    font-size: 10px;
+    letter-spacing: 1px;
+    color: #A0A0A0;
+  }
+
+  *[class*="-bar"] {
+    border-bottom: 1px solid #EBEBEB;
+  }
+
+  body{
+    max-width: 100%;
+    min-width: 100%;
+    font-family: Barlow;
+    background-color: "#F1F1F1";
+  }
+
+  .container{
+    padding: 0px;
+    max-width: 100%;
+    min-width: 100%;
+  }
+
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,50 @@
+const config = require('./contentful.json');
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'contentful-demo',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/assets/css/main'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    '@nuxtjs/vuetify'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  env: {
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: config.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
+  }
+}
